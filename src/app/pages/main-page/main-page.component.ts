0804364aa3f64af039c6ae1660e61933be2e28d9
juliebefana <marcusjuliahiller@gmail.com>
2024-04-19 15:05:41 +0200

    some small adjustments

--- src/app/pages/main-page/main-page.component.ts
+++ src/app/pages/main-page/main-page.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { ContactFormComponent } from '../../components/contact-form/contact-form.component';
 import { FaqSectionComponent } from '../../components/faq-section/faq-section.component';
+import { MatCardModule } from '@angular/material/card';
+import { MatList } from '@angular/material/list';
+import { MatListItem } from '@angular/material/list';
 
 
 @Component({
   selector: 'app-main-page',
   standalone: true,
-  imports: [ContactFormComponent, FaqSectionComponent],
+  imports: [ContactFormComponent, FaqSectionComponent, MatCardModule, MatList, MatListItem],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.css'
 })
--- src/app/pages/services-page/services-page.component.ts
+++ src/app/pages/services-page/services-page.component.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatButtonModule } from '@angular/material/button';
+import { MatStepperModule } from '@angular/material/stepper';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 
 @Component({
@@ -10,7 +11,7 @@ import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
   templateUrl: './services-page.component.html',
   styleUrls: ['./services-page.component.css'],
   standalone: true,
-  imports: [CommonModule, MatCardModule, MatGridListModule, MatButtonModule]
+  imports: [CommonModule, MatCardModule, MatGridListModule, MatButtonModule, MatStepperModule]
 })
 export class ServicesPageComponent implements OnInit {
   cols: number = 3;
