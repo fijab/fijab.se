93922379dc6c4f58bc303dc2dbfdcdbeb5055de6
juliebefana <marcusjuliahiller@gmail.com>
2024-04-24 09:52:10 +0200

    little more responsiveness

--- src/app/pages/contact-page/contact-page.component.ts
+++ src/app/pages/contact-page/contact-page.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { ContactFormComponent } from '../../components/contact-form/contact-form.component';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-page',
   standalone: true,
-  imports: [ContactFormComponent],
+  imports: [
+    ContactFormComponent,
+    CommonModule,
+    MatInputModule,
+    MatButtonModule,
+    ReactiveFormsModule],
   templateUrl: './contact-page.component.html',
   styleUrl: './contact-page.component.css'
 })
