81db6c7b3bdb619b574e87b21123241da083e018
juliebefana <marcusjuliahiller@gmail.com>
2024-05-23 11:24:42 +0200

    Added Scroll to top button

---
+++ src/app/components/scroll-to-top/scroll-to-top.component.ts
@@ -0,0 +1,23 @@
+import { Component, HostListener } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-scroll-to-top',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './scroll-to-top.component.html',
+  styleUrl: './scroll-to-top.component.css'
+})
+export class ScrollToTopComponent {
+  isVisible = false;
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    this.isVisible = window.scrollY > 300;
+  }
+
+  scrollToTop() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }
+
+}
--- src/app/pages/main-page/main-page.component.ts
+++ src/app/pages/main-page/main-page.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { ContactFormComponent } from '../../components/contact-form/contact-form.component';
 import { FaqSectionComponent } from '../../components/faq-section/faq-section.component';
-
+import { ScrollToTopComponent } from '../../components/scroll-to-top/scroll-to-top.component';
 
 
 @Component({
   selector: 'app-main-page',
   standalone: true,
-  imports: [ContactFormComponent, FaqSectionComponent],
+  imports: [ContactFormComponent, FaqSectionComponent, ScrollToTopComponent],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.css'
 })
